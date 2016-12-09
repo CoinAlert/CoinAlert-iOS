069238577ff4b7502a86109093bd14bded21d9e8
Brady Sullivan <brady@bsull.com>
2016-12-09 13:54:56 -0800

    Initial commit.

---
+++ CoinAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinAlert
+//
+//  Created by Brady Sullivan on 12/7/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
