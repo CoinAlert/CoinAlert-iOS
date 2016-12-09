589c39a895dccdc4454f4f6d98520ede2162273d
Brady Sullivan <brady@bsull.com>
2016-12-09 13:55:06 -0800

    Add basic UI elements.

--- CoinAlert/ViewController.swift
+++ CoinAlert/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var timeBar: UIProgressView!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
